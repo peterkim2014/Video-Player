8cc59baf18c6c764eb773faf3471a3840e3e05be
peterkim2014 <peterkim2014@gmail.com>
2022-11-12 13:01:45 -0500

    formatted elements in script.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 // Get our elements
-const player = document.querySelector(".player");
-const video = player.querySelector(".viewer");
-const progress = player.querySelector(".progress");
-const progressBar = player.querySelector(".progress_filled");
-const toggle = player.querySelector(".toggle");
-const skipButtons = player.querySelector("[data-skip]");
-const ranges = player.querySelector(".player_slider");
+    const player = document.querySelector(".player");
+    const video = player.querySelector(".viewer");
+    const progress = player.querySelector(".progress");
+    const progressBar = player.querySelector(".progress_filled");
+    const toggle = player.querySelector(".toggle");
+    const skipButtons = player.querySelector("[data-skip]");
+    const ranges = player.querySelector(".player_slider");
 
 // Build our functions
 
