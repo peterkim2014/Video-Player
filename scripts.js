00a001421a1ab9e161d9cc1dd6135ed6d0f14a96
peterkim2014 <peterkim2014@gmail.com>
2022-11-13 20:44:01 -0500

    added function to click on time progress bar

--- scripts.js
+++ scripts.js
@@ -35,6 +35,11 @@
         progressBar.style.flexBasis = `${percent}%`;
     };
 
+    function scrub (e) {
+        const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+        video.currentTime = scrubTime;
+    };
+
 // Hook up the event listeners
 
     video.addEventListener("click", togglePlay);
@@ -47,3 +52,5 @@
 
     ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
     ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+
+    progress.addEventListener("click", scrub);
