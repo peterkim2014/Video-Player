f897dcdc39701d40c7fceeae3a2ae97cca6f6c36
peterkim2014 <peterkim2014@gmail.com>
2022-11-13 20:37:44 -0500

    added ranges to playback speed/volume

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 // Get our elements
+
     const player = document.querySelector(".player");
     const video = player.querySelector(".viewer");
     const progress = player.querySelector(".progress");
     const progressBar = player.querySelector(".progress_filled");
     const toggle = player.querySelector(".toggle");
     const skipButtons = player.querySelectorAll("[data-skip]");
-    const ranges = player.querySelector(".player_slider");
+    const ranges = player.querySelectorAll(".player_slider");
 
 // Build our functions
 
@@ -21,7 +22,13 @@
 
     function skip () {
         video.currentTime += parseFloat(this.dataset.skip);
-    }
+    };
+
+    function handleRangeUpdate () {
+        // console.log(this.name);
+        // console.log(this.value);
+        video[this.name] = this.value;
+    };
 
 // Hook up the event listeners
 
@@ -31,3 +38,6 @@
 
     toggle.addEventListener("click", togglePlay);
     skipButtons.forEach(button => button.addEventListener("click", skip));
+
+    ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+    ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
